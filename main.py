891afd66a38a4dcec57ecadd9c18fdc60942170a
Álvaro <alvaro130303@gmail.com>
2025-03-28 09:54:48 +0100

    main creado con consultas

---
+++ main.py
@@ -0,0 +1,20 @@
+from Punto import Punto
+from Rectangulo import Rectangulo
+
+if __name__ == "__main__":
+    punto = Punto()
+    rectangulo = Rectangulo()
+
+    A = punto.crear(2, 3)
+    B = punto.crear(5, 5)
+    C = punto.crear(-3, -1)
+    D = punto.crear()
+
+    print(A)
+    print(B)
+    print(C)
+    print(D)
+
+    print(A.cuadrante())
+    print(B.cuadrante())
+    print(D.cuadrante())
