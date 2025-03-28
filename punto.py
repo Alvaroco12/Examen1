d87e783e526d3533326a92d07cb55dc13dd2a7d9
Álvaro <alvaro130303@gmail.com>
2025-03-28 09:26:48 +0100

    metodo cuadrante

---
+++ punto.py
@@ -0,0 +1,32 @@
+class Punto:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"Punto(x={self.x}, y={self.y})"
+
+    def __str__(self):
+        return f"({self.x},{self.y})"
+
+    # Métodos de clase
+    @classmethod
+    def crear(cls, x=0, y=0):
+        return cls(x, y)
+
+    # Método para determinar el cuadrante
+    def cuadrante(self):
+        if self.x > 0 and self.y > 0:
+            return "Primer cuadrante"
+        elif self.x < 0 and self.y > 0:
+            return "Segundo cuadrante"
+        elif self.x < 0 and self.y < 0:
+            return "Tercer cuadrante"
+        elif self.x > 0 and self.y < 0:
+            return "Cuarto cuadrante"
+        elif self.x == 0 and self.y != 0:
+            return "Sobre el eje Y"
+        elif self.x != 0 and self.y == 0:
+            return "Sobre el eje X"
+        else:
+            return "En el origen"
