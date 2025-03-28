a6a5725433a6818d20ead914fa9eccd3bd2ea7d5
Álvaro <alvaro130303@gmail.com>
2025-03-28 09:45:01 +0100

    metodo base y altura

--- Rectangulo.py
+++ Rectangulo.py
@@ -2,4 +2,9 @@ class Rectangulo:
     def __init__(self, p1=(0, 0), p2=(0, 0)):
         self.p1 = p1
         self.p2 = p2
-            
+    
+    def base(self):
+        return abs(self.p2[0] - self.p1[0])
+    
+    def altura(self):
+        return abs(self.p2[1] - self.p1[1])
